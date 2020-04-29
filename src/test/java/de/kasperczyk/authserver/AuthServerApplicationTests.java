4a7c81ce210d645c3131c6a1160a72bdc95d335f
Rene Kasperczyk <kasperczyk.rene@gmail.com>
2020-04-29 11:54:34 +0200

    initial commit after creating the project with the spring boot initializr

---
+++ src/test/java/de/kasperczyk/authserver/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kasperczyk.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
